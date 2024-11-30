d51dfc10e928c7d34e6ad442d1a0c8f1aeaad265
ffozilbek <fozilbekfayzullayev05@gmail.com>
2024-12-01 00:41:24 +0500

    add font and change body colors

--- js/script.js
+++ js/script.js
@@ -2,11 +2,83 @@
 window.addEventListener("DOMContentLoaded", ()=> {
     const elForm = document.querySelector(".todo__form"),
         elInput = document.querySelector(".todo__input"),
-        elTodoList = document.querySelector(".todo__list");
+        elTodoList = document.querySelector(".todo__list"),
+        elTodoDate = document.querySelector(".todo__date"),
+        elTodoIsEmpty = document.querySelector(".todo__empty");
 
-        showTodo();
+    function showDate() {
+        let day = new Date().getDay(),
+            date = new Date().getDate(),
+            month = new Date().getMonth(),
+            year = new Date().getFullYear();
+
+            switch (day) {
+                case 0:
+                  day = "Sunday";
+                  break;
+                case 1:
+                  day = "Monday";
+                  break;
+                case 2:
+                   day = "Tuesday";
+                  break;
+                case 3:
+                  day = "Wednesday";
+                  break;
+                case 4:
+                  day = "Thursday";
+                  break;
+                case 5:
+                  day = "Friday";
+                  break;
+                case 6:
+                  day = "Saturday";
+              }
+
+              switch (month) {
+                case 1:
+                  month = "Jan";
+                  break;
+                case 2:
+                  month = "Feb";
+                  break;
+                case 3:
+                   month = "Mar";
+                  break;
+                case 4:
+                  month = "Apr";
+                  break;
+                case 5:
+                  month = "May";
+                  break;
+                case 6:
+                  month = "Jun";
+                  break;
+                case 7:
+                  month = "Jul";
+                case 8:
+                  month = "Aug";
+                  break;
+                case 9:
+                  month = "Sep";
+                  break;
+                case 10:
+                  month = "Oct";
+                  break;
+                case 11:
+                  month = "Nov";
+                case 12:
+                    month = "Dec";
+              }
+
+        elTodoDate.innerHTML = `Today is ${day} ${date} ${month} ${year}`
+    }
+
+    showDate();
+
+    showTodo();
     function addTodo() {
-        if(elInput.value) {
+        if(elInput.value.trim()) {
             // create elements
             const todoItem = document.createElement("li"),
                 itemDelBtn = document.createElement("button");
@@ -27,9 +99,19 @@ window.addEventListener("DOMContentLoaded", ()=> {
         }
     }
 
+
+    function todoIsEmpty() {
+      let todos = elTodoList.querySelectorAll("li").length;
+      todos ? elTodoIsEmpty.style.display="none" : elTodoIsEmpty.style.display="block";
+
+    }
+
     elForm.addEventListener("submit", (e)=> {
         e.preventDefault();
-        addTodo()
+        addTodo();
+
+        todoIsEmpty();
+
     })
 
     elTodoList.addEventListener("click", (e)=> {
@@ -39,6 +121,7 @@ window.addEventListener("DOMContentLoaded", ()=> {
         }
         else if(e.target.tagName === "BUTTON") {
             e.target.parentElement.remove();
+            todoIsEmpty();
             saveTodo();
         }
     })
@@ -49,6 +132,7 @@ window.addEventListener("DOMContentLoaded", ()=> {
 
     function showTodo() {
         elTodoList.innerHTML = localStorage.getItem("todos");
+        todoIsEmpty();
     }
 
 
